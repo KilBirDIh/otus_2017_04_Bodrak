6e9c771937f9bae1c9aa88b5fe471ee5783fe572
KilBirDIh <kilbirdih@gmail.com>
2017-04-02 12:12:02 +0500

    created main

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    private static int count = 1;
+    public static void main(String[] args) {
+        String hello = "Hello";
+        System.out.println(hello + " " + count);
+    }
+}
