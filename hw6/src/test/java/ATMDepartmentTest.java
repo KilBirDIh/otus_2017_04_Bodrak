63945578432c5ee8421f93e985f76a6b11c2e403
Maxim Bodrak <kilbirdih@gmail.com>
2017-05-28 11:56:04 +0500

    hw6 changed tests for department

--- hw6/src/test/java/ATMDepartmentTest.java
+++ hw6/src/test/java/ATMDepartmentTest.java
@@ -16,12 +16,13 @@ public class ATMDepartmentTest
         atm.increaseBalance(100, 4);
         atm.increaseBalance(500, 6);
         department.addATM(atm);
-        ATM atm1 = new ATM(ExchangeRate.RUB, new Cell(1000), new Cell(500), new Cell(100));
-        atm1.increaseBalance(1000, 3);
-        atm1.increaseBalance(100, 1);
-        atm1.increaseBalance(500, 2);
+        ATM atm1 = new ATM(ExchangeRate.USD, new Cell(100), new Cell(50), new Cell(10), new Cell(20));
+        atm1.increaseBalance(100, 3);
+        atm1.increaseBalance(10, 1);
+        atm1.increaseBalance(50, 2);
+        atm1.increaseBalance(20, 5);
         department.addATM(atm1);
-        Assert.assertEquals(8500, department.getMargin());
+        Assert.assertEquals(4400 + 510 * ExchangeRate.USD.getMultiplier(), department.getMargin());
 
     }
 
@@ -34,10 +35,11 @@ public class ATMDepartmentTest
         atm.increaseBalance(100, 4);
         atm.increaseBalance(500, 6);
         department.addATM(atm);
-        ATM atm1 = new ATM(ExchangeRate.RUB, new Cell(1000), new Cell(500), new Cell(100));
-        atm1.increaseBalance(1000, 3);
-        atm1.increaseBalance(100, 1);
-        atm1.increaseBalance(500, 2);
+        ATM atm1 = new ATM(ExchangeRate.USD, new Cell(100), new Cell(50), new Cell(10), new Cell(20));
+        atm1.increaseBalance(100, 3);
+        atm1.increaseBalance(10, 1);
+        atm1.increaseBalance(50, 2);
+        atm1.increaseBalance(20, 5);
         department.addATM(atm1);
         department.restoreATMs();
         Assert.assertEquals(0, department.getMargin());
